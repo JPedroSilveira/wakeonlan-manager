299a278c91a1946a9434b6bbc5104b161d6e9d95
João Pedro <jpedross1999@gmail.com>
2022-08-12 20:12:43 -0300

    refactor: extract correct code

--- src/machine.cpp
+++
@@ -1,6 +0,0 @@
-/**************************************************
- * Save and provide machine information
- * - Collects and provides current machine 
- * information.
- * Provides an UDP connection port to retrieve data.
-**************************************************/
--- src/waker.cpp
+++
@@ -1,5 +0,0 @@
-/**************************************************
- * Provides and UDP connection to receive 
- * machines that must be woken up, and use 
- * wakeonlan command to wake them up
-**************************************************/
